1e13d229a960910a4e34c19d46e5205bccc6d12e
alifawaz <ali.fawaz.0120@gmail.com>
2023-07-14 18:28:30 +0300

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express')
-// const{
-//     getUsers,
-//     getUser,
-//     createUser,
-//     deleteUser,
-//     updateUser
-// } = require('../controllers/userController')
-// const router = express.Router()
+const{
+    getUsers,
+    getUser,
+    createUser,
+    deleteUser,
+    updateUser
+} = require('../controllers/userController')
+const router = express.Router()
 
 //GET all users
 router.get('/',getUsers)
