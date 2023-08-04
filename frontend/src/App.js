94cb0e4d6a7a47a53003e5d96f42ccb30f54def8
ali <alizreik1810@gmail.com>
2023-08-04 23:56:05 +0300

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,40 @@
+import React from 'react'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+
 import Hero from "./component/Hero";
 import Navbar from "./component/Navbar";
 import About from "./component/About";
 import Support from "./component/Support";
 import Allinone from "./component/Allinone";
 import Footer from './component/Footer';
-import Contactus from '../src/Contactus'
+import Contactus from './component/Contactus'
+import { element } from 'prop-types';
+
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<div> <Navbar/>
+    <Hero/>
+    <About/>  
+    <Support/>
+    <Allinone/>
+    <Footer/> </div>
+  },
+  {
+    path:'/Contactus',
+    element:<div><Contactus/></div>
+  },
+
+])
 
 function App() {
   return (
   <>
-  <Navbar/>
-  <Hero/>
-  <About/>  
-  <Support/>
-  <Allinone/>
-  <Footer/> 
-  <Contactus/>
+ 
+  
+  <RouterProvider router={router}  ><Contactus/></RouterProvider>
 
   </>
   );
